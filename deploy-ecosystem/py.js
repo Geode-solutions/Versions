21d97161913007db5466ebeb1d2ba58289bc0d4b
Arnaud Botella <arnaud.botella@geode-solutions.com>
2025-08-07 12:04:19 +0200

    edit py deps

--- deploy-ecosystem/py.js
+++ deploy-ecosystem/py.js
@@ -10,12 +10,14 @@ export default async function py_deploy(octokit, ref) {
   const vease_back = ogw_back.then(() => {
     return deploy_repository(octokit, "Geode-solutions", "Vease-Back", ref);
   });
-  const ogw_viewer = deploy_repository(
-    octokit,
-    "Geode-solutions",
-    "OpenGeodeWeb-Viewer",
-    ref
-  );
+  const ogw_viewer = ogw_back.then(() => {
+    return deploy_repository(
+      octokit,
+      "Geode-solutions",
+      "OpenGeodeWeb-Viewer",
+      ref
+    );
+  });
   const vease_viewer = ogw_viewer.then(() => {
     return deploy_repository(octokit, "Geode-solutions", "Vease-Viewer", ref);
   });

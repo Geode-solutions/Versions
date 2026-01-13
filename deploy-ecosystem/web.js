0f124564b672609a4021247cd9e20ddb0663578c
Arnaud Botella <arnaud.botella@geode-solutions.com>
2026-01-13 14:30:45 +0100

    add ext

--- deploy-ecosystem/web.js
+++ deploy-ecosystem/web.js
@@ -18,6 +18,14 @@ export default async function web_deploy(octokit, ref) {
   const vease_back = ogw_back.then(() => {
     return deploy_repository(octokit, "Geode-solutions", "Vease-Back", ref);
   });
+  const vease_modeling_back = ogw_back.then(() => {
+    return deploy_repository(
+      octokit,
+      "Geode-solutions",
+      "Vease-Modeling-Back",
+      ref
+    );
+  });
   const pegghy_back = ogw_back.then(() => {
     return deploy_repository(octokit, "Geode-solutions", "PEGGHy-Back", ref);
   });
@@ -46,10 +54,20 @@ export default async function web_deploy(octokit, ref) {
   const vease = Promise.all([vease_back, vease_viewer, ogw_front]).then(() => {
     return deploy_repository(octokit, "Geode-solutions", "Vease", ref);
   });
+  const vease_modeling = Promise.all([vease_modeling_back, ogw_front]).then(
+    () => {
+      return deploy_repository(
+        octokit,
+        "Geode-solutions",
+        "Vease-Modeling",
+        ref
+      );
+    }
+  );
   const pegghy = Promise.all([pegghy_back, pegghy_viewer, ogw_front]).then(
     () => {
       return deploy_repository(octokit, "Geode-solutions", "PEGGHy", ref);
     }
   );
-  await Promise.all([vease, pegghy]);
+  await Promise.all([vease, pegghy, vease_modeling]);
 }

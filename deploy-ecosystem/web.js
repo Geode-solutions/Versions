eb9aa2ac17f030722c03da3f82d2ba5f47338966
Arnaud Botella <arnaud.botella@geode-solutions.com>
2026-09-21 09:18:04 +0200

    update web deploy deps

--- deploy-ecosystem/web.js
+++ deploy-ecosystem/web.js
@@ -5,37 +5,22 @@ export default async function web_deploy(octokit, ref) {
     octokit,
     "Geode-solutions",
     "OpenGeodeWeb-Microservice",
-    ref
+    ref,
   );
   const ogw_back = ogw_microservice.then(() => {
-    return deploy_repository(
-      octokit,
-      "Geode-solutions",
-      "OpenGeodeWeb-Back",
-      ref
-    );
+    return deploy_repository(octokit, "Geode-solutions", "OpenGeodeWeb-Back", ref);
   });
   const vease_back = ogw_back.then(() => {
     return deploy_repository(octokit, "Geode-solutions", "Vease-Back", ref);
   });
   const vease_modeling_back = ogw_back.then(() => {
-    return deploy_repository(
-      octokit,
-      "Geode-solutions",
-      "Vease-Modeling-Back",
-      ref
-    );
+    return deploy_repository(octokit, "Geode-solutions", "Vease-Modeling-Back", ref);
   });
   const pegghy_back = ogw_back.then(() => {
     return deploy_repository(octokit, "Geode-solutions", "PEGGHy-Back", ref);
   });
   const ogw_viewer = ogw_microservice.then(() => {
-    return deploy_repository(
-      octokit,
-      "Geode-solutions",
-      "OpenGeodeWeb-Viewer",
-      ref
-    );
+    return deploy_repository(octokit, "Geode-solutions", "OpenGeodeWeb-Viewer", ref);
   });
   const vease_viewer = ogw_viewer.then(() => {
     return deploy_repository(octokit, "Geode-solutions", "Vease-Viewer", ref);
@@ -44,30 +29,16 @@ export default async function web_deploy(octokit, ref) {
     return deploy_repository(octokit, "Geode-solutions", "PEGGHy-Viewer", ref);
   });
   const ogw_front = Promise.all([ogw_back, ogw_viewer]).then(() => {
-    return deploy_repository(
-      octokit,
-      "Geode-solutions",
-      "OpenGeodeWeb-Front",
-      ref
-    );
+    return deploy_repository(octokit, "Geode-solutions", "OpenGeodeWeb-Front", ref);
   });
   const vease = Promise.all([vease_back, vease_viewer, ogw_front]).then(() => {
     return deploy_repository(octokit, "Geode-solutions", "Vease", ref);
   });
-  const vease_modeling = Promise.all([vease_modeling_back, ogw_front]).then(
-    () => {
-      return deploy_repository(
-        octokit,
-        "Geode-solutions",
-        "Vease-Modeling",
-        ref
-      );
-    }
-  );
-  const pegghy = Promise.all([pegghy_back, pegghy_viewer, ogw_front]).then(
-    () => {
-      return deploy_repository(octokit, "Geode-solutions", "PEGGHy", ref);
-    }
-  );
+  const vease_modeling = Promise.all([vease_modeling_back, vease]).then(() => {
+    return deploy_repository(octokit, "Geode-solutions", "Vease-Modeling", ref);
+  });
+  const pegghy = Promise.all([pegghy_back, pegghy_viewer, ogw_front]).then(() => {
+    return deploy_repository(octokit, "Geode-solutions", "PEGGHy", ref);
+  });
   await Promise.all([vease, pegghy, vease_modeling]);
 }
